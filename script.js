d110fcb4a1ce453cab68f9d59a78a303852ff696
Raghav jain <101123378+raghav1230@users.noreply.github.com>
2023-07-27 15:11:14 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-document.querySelector('.menu').addEventListener('click', function() {
-    var navbar = document.getElementById('navbar');
-    if (navbar.style.display === 'block') {
-        navbar.style.display = 'none';
-    } else {
-        navbar.style.display = 'block';
-    }
-});
